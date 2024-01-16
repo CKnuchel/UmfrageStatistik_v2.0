8256cf3da556db97ceb106b82a27510fa2a47717
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-01-16 16:04:16 +0100

    ADDED: Implementiert des ModulRepositorys

---
+++ Logic/Repository/ModulRepository.cs
@@ -0,0 +1,32 @@
+﻿using Common.Models;
+using Data.Context;
+using Logic.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logic.Repository;
+
+public class ModulRepository : IModulRepository
+{
+    #region Fields
+    private readonly UmfrageContext _context;
+    #endregion
+
+    #region Constructors
+    public ModulRepository(UmfrageContext context)
+    {
+        _context = context;
+    }
+    #endregion
+
+    #region Publics
+    public async Task<List<Modul>> GetAllModuleAsync()
+    {
+        return await _context.Module.ToListAsync() ?? throw new NullReferenceException("Es existieren keine Einträge");
+    }
+
+    public async Task<Modul> GetModuleByIdAsync(int modulId)
+    {
+        return await _context.Module.FindAsync(modulId) ?? throw new NullReferenceException("Diese Id existiert nicht.");
+    }
+    #endregion
+}
