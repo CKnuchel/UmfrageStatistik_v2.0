0db6f05bf1d7f89c9d4f7cd0ce112660b6c8a615
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-01-16 12:31:51 +0100

    Add project files.

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("M&S Software Engineering AG")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © M&S Software Engineering AG 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9246d14d-06a8-4644-9278-1cd3ee9b296a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
