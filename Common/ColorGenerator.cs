e5e1c6d6ca9a8ca2cba6b3e75f8cc5ad78cd4675
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-01-30 15:25:10 +0100

    ADDED: Color Generator

---
+++ Common/ColorGenerator.cs
@@ -0,0 +1,88 @@
+﻿namespace Common;
+
+public static class ColorGenerator
+{
+    #region Publics
+    public static string[] CategoricalSixColors()
+    {
+        return new string[6]
+               {
+                   "#0fb5ae",
+                   "#4046ca",
+                   "#f68511",
+                   "#de3d82",
+                   "#7e84fa",
+                   "#72e06a"
+               };
+    }
+
+    public static string[] CategoricalTwelveColors()
+    {
+        return new string[12]
+               {
+                   "#0fb5ae",
+                   "#4046ca",
+                   "#f68511",
+                   "#de3d82",
+                   "#7e84fa",
+                   "#72e06a",
+                   "#147af3",
+                   "#7326d3",
+                   "#e8c600",
+                   "#cb5d00",
+                   "#008f5d",
+                   "#bce931"
+               };
+    }
+
+    public static string[] CategoricalSixteenColors()
+    {
+        return new string[16]
+               {
+                   "#0fb5ae", // Türkis
+                   "#4046ca", // Dunkelblau
+                   "#f68511", // Orange
+                   "#de3d82", // Pink
+                   "#7e84fa", // Hellblau
+                   "#72e06a", // Hellgrün
+                   "#147af3", // Königsblau
+                   "#7326d3", // Lila
+                   "#e8c600", // Gelb
+                   "#cb5d00", // Braun
+                   "#008f5d", // Dunkelgrün
+                   "#bce931", // Limettengrün
+                   "#ff69b4", // Helles Pink
+                   "#a52a2a", // Kastanienbraun
+                   "#ffd700", // Gold
+                   "#00ced1" // Dunkles Türkis
+               };
+    }
+
+    public static string[] CategoricalTwentyColors()
+    {
+        return new string[20]
+               {
+                   "#0fb5ae", // Türkis
+                   "#4046ca", // Dunkelblau
+                   "#f68511", // Orange
+                   "#de3d82", // Pink
+                   "#7e84fa", // Hellblau
+                   "#72e06a", // Hellgrün
+                   "#147af3", // Königsblau
+                   "#7326d3", // Lila
+                   "#e8c600", // Gelb
+                   "#cb5d00", // Braun
+                   "#008f5d", // Dunkelgrün
+                   "#bce931", // Limettengrün
+                   "#ff69b4", // Helles Pink
+                   "#a52a2a", // Kastanienbraun
+                   "#ffd700", // Gold
+                   "#00ced1", // Dunkles Türkis
+                   "#c71585", // Mittleres Violettrot
+                   "#808080", // Grau
+                   "#ffa500", // Orange
+                   "#800000" // Maroon (Dunkelrot)
+               };
+    }
+    #endregion
+}
