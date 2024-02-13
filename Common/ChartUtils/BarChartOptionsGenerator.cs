a4f93b222268d43e9a8be84e6fbd9c61dd94e27e
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-02-13 09:48:31 +0100

    INFO: Added Summary

--- Common/ChartUtils/BarChartOptionsGenerator.cs
+++ Common/ChartUtils/BarChartOptionsGenerator.cs
@@ -9,14 +9,22 @@ public class BarChartOptionsGenerator
     #endregion
 
     #region Constructors
+    /// <summary>
+    /// Optionen für das BarChart mit den nötigsten Default Werten
+    /// </summary>
+    /// <param name="indexAxis">x = vertikal, y = Horizontal</param>
+    /// <param name="xTitle">Titel der X-Achse</param>
+    /// <param name="yTitle">Titel der Y-Achse</param>
     public BarChartOptionsGenerator(string indexAxis, string xTitle, string yTitle)
     {
         CreateDefaultChartOptions();
-        options.IndexAxis = indexAxis.ToLower();
-        options.Scales.X.Title.Text = xTitle;
-        options.Scales.Y.Title.Text = yTitle;
+        options!.IndexAxis = indexAxis.ToLower();
+        options.Scales.X!.Title!.Text = xTitle;
+        options.Scales.Y!.Title!.Text = yTitle;
     }
+    #endregion
 
+    #region Publics
     public BarChartOptions GetOptions()
     {
         return options;
@@ -29,17 +37,16 @@ public class BarChartOptionsGenerator
         options = new BarChartOptions
                   {
                       Responsive = true,
-                      Interaction = { Mode = InteractionMode.X },
+                      Interaction = new Interaction { Mode = InteractionMode.X },
                   };
 
-        options.Scales.X.Title.Font!.Size = 24;
-        options.Scales.X.Title.Display = true;
+        options.Scales.X!.Title!.Font!.Size = 24;
+        options.Scales.X!.Title!.Display = true;
 
-        options.Scales.Y.Title.Font!.Size = 24;
-        options.Scales.Y.Title.Display = true;
+        options.Scales.Y!.Title!.Font!.Size = 24;
+        options.Scales.Y!.Title!.Display = true;
 
         options.Plugins.Legend.Display = false;
     }
-
     #endregion
 }
