ce3343eb9282d75b168dfe5d14a7751dec09d638
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-02-16 15:57:54 +0100

    INFO: Commit am Ende der Woche

--- Web/Pages/Semester.razor.cs
+++ Web/Pages/Semester.razor.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorBootstrap;
+using Data.Context;
+using Logic.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace Web.Pages;
 
 public partial class Semester : ComponentBase
 {
-    public List<string> test = new List<string>();
+    #region Fields
+    public List<string> test = new();
+    public List<BlazorBootstrapChart> chartList = new();
+
+    private PieChart testChart1 = new PieChart();
+    private PieChart testChart2 = new PieChart();
+    #endregion
+
+    #region Properties
+    [Inject]
+    public IDbContextFactory<UmfrageContext> ContextFactory { get; set; } = default!;
+
+    [Inject]
+    private IBasicLoader BasicLoader { get; set; } = default!;
+    #endregion
+
+    #region Protecteds
     protected override void OnInitialized()
     {
         for(int i = 1; i <= 20; i++)
@@ -12,4 +32,19 @@ public partial class Semester : ComponentBase
             test.Add($"Ich bin die {i}. Zahl in der Liste.");
         }
     }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await AddTestCharts();
+    }
+    #endregion
+
+    #region Privates
+    private async Task AddTestCharts()
+    {
+        // https://docs.blazorbootstrap.com/data-visualization/pie-chart Beispiel mit daten hinzufügen umdiese allenfalls später zu laden
+
+        throw new NotImplementedException();
+    }
+    #endregion
 }
