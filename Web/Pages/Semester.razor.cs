a12b59b3e8db27da2ca065efe376411e63d98ff4
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-02-20 10:20:08 +0100

    ADDED: Loading for Modulfilter (Dropdown)

--- Web/Pages/Semester.razor.cs
+++ Web/Pages/Semester.razor.cs
@@ -1,15 +1,34 @@
 ﻿using BlazorBootstrap;
 using Common.ChartUtils;
+using Common.Models;
+using Logic.Interfaces;
 using Microsoft.AspNetCore.Components;
 
 namespace Web.Pages;
 
 public partial class Semester : ComponentBase
 {
+    #region Constants
+    private static readonly Modul DefaultModul = new() { Id = 0, Name = "Alle Module" };
+    #endregion
+
     #region Properties
+    public Modul SelectedModul { get; set; } = DefaultModul;
+    public List<Modul> ModuleList { get; set; } = new();
     public BarChart BarChart { get; set; } = new();
     public BarChartOptions BarChartOptions { get; set; } = new();
     public ChartData BarChartData { get; set; } = new();
+
+    [Inject]
+    public IRepository<Modul> ModulRepository { get; set; } = default!;
+    #endregion
+
+    #region Publics
+    public async Task SetSelectedModul(Modul modul)
+    {
+        this.SelectedModul = modul;
+        await LoadDataBasedOnSelectionAsync();
+    }
     #endregion
 
     #region Protecteds
@@ -30,6 +49,13 @@ public partial class Semester : ComponentBase
     #endregion
 
     #region Privates
+    private async Task LoadDataBasedOnSelectionAsync()
+    {
+        //await DetermineChartDataAsync();
+        //await UpdateDisplayedChartsAsync();
+        StateHasChanged();
+    }
+
     private void InitializeChartOptions()
     {
         this.BarChartOptions = new BarChartOptionsGenerator("x", "Fragen", "Anzahl Antworten").GetOptions();
@@ -37,7 +63,10 @@ public partial class Semester : ComponentBase
 
     private async Task LoadInitialDataAsync()
     {
-        // TODO Allenfalls Filter Listen
+        // Modul Filter laden
+        this.ModuleList.Add(DefaultModul);
+        List<Modul> loadedModulesTask = await this.ModulRepository.GetAllAsync();
+        this.ModuleList.AddRange(loadedModulesTask);
         // TODO Semster Loader From BarChart Loader
     }
     #endregion
